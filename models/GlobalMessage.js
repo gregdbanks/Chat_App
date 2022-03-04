d4038e79988ccc4c005e83a1e68fc6ba7f9ab9bc
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-03-04 00:45:35 -0600

    Fixing Date issue

--- models/GlobalMessage.js
+++ models/GlobalMessage.js
@@ -3,21 +3,21 @@ const Schema = mongoose.Schema;
 
 // Create Schema for Users
 const GlobalMessageSchema = new Schema({
-    from: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-    },
-    body: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        default: Date.now,
-    },
+  from: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = GlobalMessage = mongoose.model(
-    'global_messages',
-    GlobalMessageSchema
+  'global_messages',
+  GlobalMessageSchema
 );
--- models/User.js
+++ models/User.js
@@ -3,22 +3,22 @@ const Schema = mongoose.Schema;
 
 // Create Schema for Users
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
